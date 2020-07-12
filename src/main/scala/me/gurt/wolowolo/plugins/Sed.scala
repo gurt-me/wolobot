8502562dcc9164871acb05f3af09c5336a204bd2
Qi Ji <qjqqyy@gmail.com>
2020-07-12 10:15:34 +0800

    pull out line storage related stuff

--- src/main/scala/me/gurt/wolowolo/plugins/Sed.scala
+++ src/main/scala/me/gurt/wolowolo/plugins/Sed.scala
@@ -10,34 +10,8 @@ import scala.util.matching.Regex
 
 class Sed extends Plugin {
   import Sed._
-  private var lines: LineStorage =
-    SortedMap.empty(Ordering.comparatorToOrdering(String.CASE_INSENSITIVE_ORDER))
 
-  private def unshiftLine(key: String, line: String): Unit =
-    synchronized {
-      lines = lines.updatedWith(key) {
-        case Some(queue) => Some(queue.finPrepended(line))
-        case None        => Some(Queue(line))
-      }
-    }
-
-  private def findAndReplaceLine(
-      key: String,
-      search: String,
-      replacement: String,
-      flags: String,
-  ): Option[(String, String)] = {
-    val insensitiveFlag = if (flags.contains('i')) """(?i)""" else ""
-    val regex           = s"$insensitiveFlag$search".r.unanchored
-    val replaceWith: String => String => String = repl =>
-      src =>
-        if (flags.contains('g')) regex.replaceAllIn(src, repl)
-        else regex.replaceFirstIn(src, repl)
-
-    lines(key) find (regex.matches) map { line =>
-      (replaceWith(replacement)(line), replaceWith(s"$BOLD$replacement$BOLD")(line))
-    }
-  }
+  private val ls = LineStorage(4)
 
   def handle(net: String): Handler = {
     case (UserInfo(nick, _, _), t @ Channel(chan), PrivMsg(_, msg)) =>
@@ -45,13 +19,18 @@ class Sed extends Plugin {
       // ahahah fuck you type erasure
       val (toStore, toSend) = Generic[(Option[String], Option[Sendable])].from(msg match {
         case sedMyself(_, search, replacement, flags) =>
-          findAndReplaceLine(s"$net.$chan.$nick", search, replacement, Option(flags) getOrElse "")
+          ls.findAndReplaceLine(
+              s"$net.$chan.$nick",
+              search,
+              replacement,
+              Option(flags) getOrElse "",
+            )
             .map {
               case (toStore, toReply) => HList(Some(toStore), Some(t msg s"<$nick> $toReply"))
             } getOrElse HList(None, None)
 
         case sedOther(who, _, search, replacement, flags) =>
-          findAndReplaceLine(s"$net.$chan.$who", search, replacement, Option(flags) getOrElse "")
+          ls.findAndReplaceLine(s"$net.$chan.$who", search, replacement, Option(flags) getOrElse "")
             .map {
               case (_, toReply) => HList(None, Some(t msg s"$nick thinks $who meant: $toReply"))
             } getOrElse HList(None, None)
@@ -59,7 +38,7 @@ class Sed extends Plugin {
         case _ =>
           HList(Some(msg), None)
       })
-      toStore.foreach(unshiftLine(s"$net.$chan.$nick", _))
+      toStore.foreach(ls.unshiftLine(s"$net.$chan.$nick", _))
       toSend
     case _ =>
       None
@@ -68,18 +47,46 @@ class Sed extends Plugin {
 }
 
 object Sed {
-  type LineStorage = SortedMap[String, Queue[String]]
-
   val sedMyself: Regex = """s([^a-zA-Z0-9 ])(.+?)\1(.*?)(?:\1(.*))?\s*""".r
   val sedOther: Regex =
     """([-\\a-zA-Z0-9_\[\]{}^`|]+)[:,] s([^a-zA-Z0-9 ])(.+?)\2(.*?)(?:\2(.*))?\s*""".r
 
-  private val maxLinesToRemember = 4
+  class LineStorage(maxLinesToRemember: Int) {
+    private var lines: SortedMap[String, Queue[String]] =
+      SortedMap.empty(Ordering.comparatorToOrdering(String.CASE_INSENSITIVE_ORDER))
 
-  implicit class FiniteQueue[T](q: Queue[T]) {
-    def finPrepended(t: T): Queue[T] = {
-      q.prepended(t).take(maxLinesToRemember)
+    def unshiftLine(key: String, line: String): Unit =
+      synchronized {
+        lines = lines.updatedWith(key) {
+          case Some(queue) => Some(queue.prepended(line).take(maxLinesToRemember))
+          case None        => Some(Queue(line))
+        }
+      }
+
+    def findAndReplaceLine(
+        key: String,
+        search: String,
+        replacement: String,
+        flags: String,
+    ): Option[(String, String)] = {
+      val insensitiveFlag = if (flags.contains('i')) """(?i)""" else ""
+      val regex           = s"$insensitiveFlag$search".r.unanchored
+      val replaceWith: String => String => String = repl =>
+        src =>
+          if (flags.contains('g')) regex.replaceAllIn(src, repl)
+          else regex.replaceFirstIn(src, repl)
+
+      lines(key) find (regex.matches) map { line =>
+        (replaceWith(replacement)(line), replaceWith(s"$BOLD$replacement$BOLD")(line))
+      }
     }
   }
 
+  object LineStorage {
+    def apply(maxLinesToRemember: Int) =
+      if (maxLinesToRemember < 1)
+        throw new IllegalArgumentException("wtf bro how do you expect me to keep 0 or less lines??")
+      else
+        new LineStorage(maxLinesToRemember)
+  }
 }
